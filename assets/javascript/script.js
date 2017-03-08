743f2d4124f8f4c207bc3946ad58ad6973961d75
navarretejuan <navarretejuan09@gmail.com>
2017-03-08 00:24:54 -0800

    add buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,22 @@
 /**
  * Created by juancarlosnavarrete on 3/7/17.
  */
+
+var apiKey = "dc6zaTOxFJmzC";
+
+var topics = ["Terry Crews", "puppy", "Eddie Murphy", "Amy WineHouse"];
+
+$(document).ready(function () {
+
+    for(var i =0; i< topics.length; i++){
+        var btn = $('<a>');
+        btn.addClass("btn btn-primary btn-lg");
+        btn.attr('role', 'button');
+        btn.text(topics[i]);
+        $(".btn-container").append(btn);
+    }
+    $(".btn-lg").on("click", function() {
+        console.log('btn click');
+    })
+});
+
