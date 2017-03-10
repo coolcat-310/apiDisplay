00ffaa360a3029f6d57f51065b2c9fbdd3a3f982
navarretejuan <navarretejuan09@gmail.com>
2017-03-10 14:37:01 -0800

    update README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # apiDisplay
+A javaScript file that retrieves GIFs from the GIPHY's library. It retrieves the 
+information through an asynchronous task called AJAX. Then displays the GIF onto 
+index.html. 
+
+```
+https://github.com/Giphy/GiphyAPI
+```
+## Live Link
+https://coolcat-310.github.io/apiDisplay/
+
+
+## Technology Used
+
+- HTML 5
+- CSS/Bootstrap 3
+- jQuery 3.1
+
+
+## Resources
+- HTML:         index.html
+- JavaScript:   assets/javascript/app.js
+- CSS:          assets/css/style.css
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -27,6 +27,11 @@ var topics = {
 };
 
 function buildInitialButtons(){
+    /**
+     * This function creates buttons based on the array in the 'topics' object.
+     * @method buildInitialButtons
+     * @param none
+     */
     var arr = topics.getPeople();
 
     for(var i =0; i< arr.length; i++){
@@ -35,6 +40,11 @@ function buildInitialButtons(){
 }
 
 function buildGiphy(arr) {
+    /**
+     * This function displays the results of the trivia game as a table and invokes the function updatePie()
+     * @method buildGiphy
+     * @param none
+     */
 
     for(var i = 0; i < arr.length; i++){
         console.log(arr[i]);
@@ -43,9 +53,14 @@ function buildGiphy(arr) {
 }
 
 function callApi(name) {
+    /**
+     * This function creates the URL and utilize AJAX to retrieve gifs from the API.
+     * @method callApi
+     * @param str: the name of the topic
+     */
 
     console.log(name);
-    var queryURL = baseURL + name + "&api_key=" + apiKey +"&limit=10";
+    var queryURL = baseURL + "" +  name + "&api_key=" + apiKey +"&limit=10";
     console.log(queryURL);
 
     $.ajax({
@@ -58,11 +73,11 @@ function callApi(name) {
             console.log(Object.keys(response.data).length)
         }
         if(Object.keys(response.data).length === 0){
-            //There are no gif for the given 'name'
-            //change color of button to red(warning)
+            //check this if statement
             console.log('inside the check');
-            $("#" + name).addClass('btn-danger').removeClass('btn-primary');
+            var checkName = topics.removePlus(name);
             alert("There are no gif for " + topics.removePlus(name));
+            $("#" + checkName).addClass('btn-danger').removeClass('btn-primary');
         }
         else {
             buildGiphy(response.data);
@@ -71,31 +86,51 @@ function callApi(name) {
 }
 
 function buildButton(newName){
+    /**
+     * This function creates a button and adds classes, attributes and id
+     * @method buildButton
+     * @param str: which will be used as the id of the button
+     */
     var btn = $('<a>');
     btn.addClass("btn btn-primary btn-lg");
-    btn.attr({'role': 'button', 'data-person': topics.removeSpace(newName), id: newName});
+    btn.attr({
+        'role': 'button',
+        'data-person': topics.removeSpace(newName),
+        id: newName
+    });
     btn.text(newName);
     $(".btn-container").append(btn);
 }
 
 function buildGif(obj){
+    /**
+     * This function creates a gif with several attributes
+     * @method buildGif
+     * @param obj: a gif object
+     */
+
     var gifDiv = $("<div class='item'>");
     var rating = obj.rating;
     var p = $("<p>").text("Rating: " + rating);
     var personImage = $("<img>");
-    personImage.attr("src", obj.images.fixed_height.url);
-    personImage.attr("data-animate", obj.images.fixed_height.url);
-    personImage.attr("data-still", obj.images.original_still.url);
-    personImage.attr("data-state", 'animate');
+    personImage.attr({
+        "src": obj.images.original_still.url,
+        "data-animate": obj.images.fixed_height.url,
+        "data-still": obj.images.original_still.url,
+        "data-state": "still"
+    })
     personImage.addClass("gif");
     gifDiv.prepend(p);
     gifDiv.prepend(personImage);
-
     $(".giphy").prepend(gifDiv);
 }
 
 
 $(document).ready(function () {
+    /**
+     * When index.html loads then this function will run.
+     * @method buildGif
+     */
     buildInitialButtons();
     if(DEBUG){
         console.log("Create Initial Buttons");
@@ -103,6 +138,10 @@ $(document).ready(function () {
 });
 
 $("#add-user").on("click", function(event) {
+    /**
+     * When the button with id:add-user is clicked then this function will run.
+     * @method click
+     */
     event.preventDefault();
     var name = $("#name-input").val();
     topics.addPerson(name);
@@ -114,8 +153,11 @@ $("#add-user").on("click", function(event) {
 });
 
 $(document.body).on("click", ".btn-primary", function() {
-
-    var person = $(this).attr("data-person");
+    /**
+     * When the button with class:btn-primary is clicked then this function will run.
+     * @method click
+     */
+    var person = $(this).attr('id');
     callApi(person);
     if(DEBUG){
         console.log("Click on this button: " + person);
@@ -123,14 +165,17 @@ $(document.body).on("click", ".btn-primary", function() {
 });
 
 $(document.body).on("click", ".gif", function () {
-
+    /**
+     * When the button with class:gif is clicked then this function will run.
+     * @method click
+     */
     if($(this).attr("data-state") === "animate"){
-        console.log("current state is change from animate to still");
+        console.log("current state has change from animate to still");
         $(this).attr("data-state", "still");
         $(this).attr("src", $(this).attr("data-still"));
     }
     else{
-        console.log("current state is still change to animate");
+        console.log("current state has change from still change to animate");
         $(this).attr("data-state", "animate");
         $(this).attr("src", $(this).attr("data-animate"));
     }
